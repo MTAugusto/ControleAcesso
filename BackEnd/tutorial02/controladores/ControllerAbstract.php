2eed05816b71b87d68be67c04a2534c22b2dc1bb
Lucas Montanheiro <lucasmontanheiro10@gmail.com>
2017-05-15 23:49:55 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ BackEnd/tutorial02/controladores/ControllerAbstract.php
@@ -0,0 +1,38 @@
+<?php 
+
+	$request_method=$_SERVER["REQUEST_METHOD"];
+
+	switch($request_method)
+	{
+		case 'GET':
+			// Retrive Products
+			if(!empty($_GET["id"]))
+			{
+				$id=intval($_GET["id"]);
+				retreave($id);
+			}
+			else
+			{
+				retreave();
+			}
+			break;
+		case 'POST':
+			// Insert Product
+			insert();
+			break;
+		case 'PUT':
+			// Update Product
+			$id=intval($_GET["id"]);
+			update($id);
+			break;
+		case 'DELETE':
+			// Delete Product
+			$id=intval($_GET["id"]);
+			delete($id);
+			break;
+		default:
+			// Invalid Request Method
+			header("HTTP/2.0 405 Method Not Allowed");
+			break;
+	}
+
