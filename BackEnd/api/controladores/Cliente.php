4acd16a7d92d78465525ab3d9ab046b67ca13e90
Lucas Montanheiro <lucasmontanheiro10@gmail.com>
2017-05-18 15:34:28 -0300

    Token e Cliente

--- BackEnd/api/controladores/Cliente.php
+++ BackEnd/api/controladores/Cliente.php
@@ -8,31 +8,27 @@ include '../controladores/Token.php';
 	switch($request_method)
 	{
 		case 'GET':
-			// Retrive Products
-			if(!empty($_GET["id"]))
-			{
-				$id=intval($_GET["id"]);
-				retreave($id);
-			}
-			else
-			{
-				retreave();
+			if(verificarLogin()){
+				if(!empty($_GET["id"]))
+				{
+					$id=intval($_GET["id"]);
+					retreave($id);
+				}
+				else retreave();
 			}
 			break;
 		case 'POST':
-			// Insert Product
 			insert();
 			break;
 		case 'PUT':
-			// Update Product
 			$id=intval($_GET["id"]);
 			update($id);
 			break;
-		case 'DELETE':
-			// Delete Product
-			$id=intval($_GET["id"]);
-			delete($id);
-			break;
+		// case 'DELETE':
+		// 	// Delete Product
+		// 	$id=intval($_GET["id"]);
+		// 	delete($id);
+		// 	break;
 		default:
 			// Invalid Request Method
 			header("HTTP/2.0 405 Method Not Allowed");
--- BackEnd/api/controladores/Token.php
+++ BackEnd/api/controladores/Token.php
@@ -1,8 +1,8 @@
 <?php
 
-ini_set('display_errors',1);
-ini_set('display_startup_erros',1);
-error_reporting(E_ALL);
+// ini_set('display_errors',1);
+// ini_set('display_startup_erros',1);
+// error_reporting(E_ALL);
 
 require_once('../vendor/autoload.php');
 
@@ -212,4 +212,3 @@ use Lcobucci\JWT\Parser;
 			}
 		}
 	}
-?>
