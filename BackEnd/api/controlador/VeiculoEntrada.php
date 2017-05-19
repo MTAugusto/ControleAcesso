d5f20dabd5d546f2ea3541b6e3ca963802a69cf3
Lucas Montanheiro <lucasmontanheiro10@gmail.com>
2017-05-19 17:24:06 -0300

    API Veiculo Entrada

---
+++ BackEnd/api/controlador/VeiculoEntrada.php
@@ -0,0 +1,38 @@
+<?php
+include '../persistencia/VeiculoEntradaDAO.php';
+include '../persistencia/Connect.php';
+include '../negocio/Token.php';
+
+	$request_method=$_SERVER["REQUEST_METHOD"];
+
+	switch($request_method)
+	{
+		case 'GET':
+			if(verificarLogin()){
+
+				if(!empty($_GET["id"]))
+				{
+					$id=intval($_GET["id"]);
+					retrieve($id);
+				}
+				else retrieve();
+			}
+			break;
+		case 'POST':
+			if(verificarLogin()) insert();
+			break;
+		// case 'PUT':
+		// 	if(verificarLogin()) update();
+		// 	break;
+		// case 'DELETE':
+		// 	// Delete Product
+		// 	$id=intval($_GET["id"]);
+		// 	delete($id);
+		// 	break;
+		default:
+			// Invalid Request Method
+			header("HTTP/2.0 405 Method Not Allowed");
+			break;
+	}
+
+?>
---
+++ BackEnd/api/persistencia/VeiculoEntradaDAO.php
@@ -0,0 +1,100 @@
+<?php
+
+	function insert()
+	{
+		date_default_timezone_set('America/Sao_Paulo');
+		global $connection;
+		$veiculo=$_POST["veiculo"];
+		$data=date("Y-m-d H:i:s", time());
+		
+		// VERIFICAR SE JÁ NÃO EXISTE UMA ENTRADA EM ABERTO PARA ESSE VEICULO
+
+		// VERIFICAR SE JÁ NÃO EXISTE UMA ENTRADA EM ABERTO PARA ESSE VEICULO
+
+		// VERIFICAR SE JÁ NÃO EXISTE UMA ENTRADA EM ABERTO PARA ESSE VEICULO
+
+		$query="INSERT INTO entradas_veiculos SET veiculo='{$veiculo}', data='{$data}'";
+		if(mysqli_query($connection, $query))
+		{
+			$response=array(
+				'status' => 1,
+				'message' =>'Adicionado com sucesso.'
+			);
+		}
+		else
+		{
+			$response=array(
+				'status' => 0,
+				'message' =>'Houve um erro ao adicionar.'
+			);
+		}
+		header('Content-Type: application/json');
+		echo json_encode($response);
+	}
+	function retrieve($id=0)
+	{
+		global $connection;
+		$query="SELECT * FROM entradas_veiculos";
+		if($id != 0)
+		{
+			$query.=" WHERE id=".$id." LIMIT 1";
+		}
+		$response=array();
+		$result=mysqli_query($connection, $query);
+		while($row=mysqli_fetch_object($result))
+		{
+			$response[]=$row;
+		}
+		header('Content-Type: application/json');
+		echo json_encode($response);
+	}
+	// function delete($id)
+	// {
+	// 	global $connection;
+	// 	$query="DELETE FROM entradas_veiculos WHERE id=".$id;
+	// 	if(mysqli_query($connection, $query))
+	// 	{
+	// 		$response=array(
+	// 			'status' => 1,
+	// 			'message' =>'Deletado com sucesso.'
+	// 		);
+	// 	}
+	// 	else
+	// 	{
+	// 		$response=array(
+	// 			'status' => 0,
+	// 			'message' =>'Houve um erro ao deletar.'
+	// 		);
+	// 	}
+	// 	header('Content-Type: application/json');
+	// 	echo json_encode($response);
+	// }
+	// function update()
+	// {
+	// 	global $connection;
+	// 	parse_str(file_get_contents("php://input"),$post_vars);
+	// 	$id=$post_vars['id'];
+	// 	$veiculo=$post_vars['veiculo'];
+	// 	$data=$post_vars["data"];
+	// 	$valorpormes=$post_vars["valorpormes"];
+	// 	$query="UPDATE entradas_veiculos SET veiculo='{$veiculo}', data='{$data}', valorpormes='{$valorpormes}' WHERE id=".$id;
+	// 	if(mysqli_query($connection, $query))
+	// 	{
+	// 		$response=array(
+	// 			'status' => 1,
+	// 			'message' =>'Atualizado com sucesso.'
+	// 		);
+	// 	}
+	// 	else
+	// 	{
+	// 		$response=array(
+	// 			'status' => 0,
+	// 			'message' =>'Houve um erro ao atualizar.'
+	// 		);
+	// 	}
+	// 	header('Content-Type: application/json');
+	// 	echo json_encode($response);
+	// }
+
+	// Close database connection
+	mysqli_close($connection);
