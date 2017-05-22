1c7fd151fd03b5677ae2d242488efde49a5877b6
Lucas Montanheiro <lucasmontanheiro10@gmail.com>
2017-05-22 13:52:20 -0300

    Tirando angualar da página principal e passando pro painel

--- web_estacionamento/js/autentica-login.js
+++ web_estacionamento/js/autentica-login.js
@@ -1,19 +1,33 @@
 function fnConsomeWs() {
+
+     let usuario = document.getElementById("usuario").value;
+     let senha = document.getElementById("password").value;
+     let ficarLogado = document.getElementById("filled-in-box").checked;
+
      $.ajax({
           type: "POST",
           url: "http://localhost/ControleAcesso/BackEnd/api/controlador/Login.php",
           contentType: "application/x-www-form-urlencoded; charset=utf-8",
           dataType: "json",
           async: true,
-          data: '{Usuario:"' + document.getElementById("usuario").value + '",Senha:"' + document.getElementById("password").value + '"}',
+          data: $.param({
+                        'usuario': usuario,
+                        'senha': senha
+                    }),
           success: function(response) {
+
+               if (ficarLogado) {
+                    localStorage.setItem('user_session', response.token);
+                    console.log("token gravado no local storage");
+               } else {
+                    sessionStorage.setItem('user_session', response.token);
+                    console.log("token gravado no session storage");
+               }
                console.log(response);
-               localStorage.setItem('user_session', response.token);
-               console.log("token gravado no local storage");
                window.location = "/painel/#/";    
           },
           error: function(response) {
-
+               console.log(response.responseJSON);
           }
      });
 }
