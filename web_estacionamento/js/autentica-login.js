bbfd8075a66e934f6376534cefb76dc9cc73ade9
Mateus Augusto <mateus_2d@hotmail.com>
2017-05-22 08:42:02 -0300

    Correção content type

--- web_estacionamento/js/autentica-login.js
+++ web_estacionamento/js/autentica-login.js
@@ -2,7 +2,7 @@ function fnConsomeWs() {
      $.ajax({
           type: "POST",
           url: "http://localhost/ControleAcesso/BackEnd/api/controlador/Login.php",
-          contentType: "application/json; charset=utf-8",
+          contentType: "application/x-www-form-urlencoded; charset=utf-8",
           dataType: "json",
           async: true,
           data: '{Usuario:"' + document.getElementById("usuario").value + '",Senha:"' + document.getElementById("password").value + '"}',
