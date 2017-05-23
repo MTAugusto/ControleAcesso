1959bd58e2e14e9be596d3e14ded245e548ef9f0
Mateus Augusto <mateus_2d@hotmail.com>
2017-05-22 22:44:44 -0300

    Alteração de nomeclatura Cliente.php para cliente

--- web_estacionamento/painel/js/clientesCtrl.js
+++ web_estacionamento/painel/js/clientesCtrl.js
@@ -1,36 +1,38 @@
 angular.module('spa')
-	.controller('clientesCtrl', ['$scope', '$rootScope', '$mdToast', '$http', '$location',
-	function($scope, $rootScope, $mdToast, $http, location){
-		$scope.name = 'Administração de Clientes';
+     .controller('clientesCtrl', ['$scope', '$rootScope', '$mdToast', '$http', '$location',
+          function($scope, $rootScope, $mdToast, $http, location) {
+               $scope.name = 'Administração de Clientes';
 
-		$scope.consultarClientes = function(){
-            var token = sessionStorage.getItem("user_session") || localStorage.getItem("user_session");
-            if(token) {
-                $http({ 
-                        url: $rootScope.api + '/Cliente.php', 
-                        method:'GET',
-                        headers: {'Authorization': token},
-                    }).success(function (response) {
-                        $scope.clientes = response;
-                        console.log(response);
-                    }).error(function (response) {                     
-                        $mdToast.show($mdToast.simple()
-                            .content(response.message)
-                            .hideDelay(3000));
-                        console.log(response);
-                        if (response.status == 2) window.location = "/#/login";
-                    });
-            }else{
-                console.log("sem token");
-                window.location = "/#/login";
-            }
-        };
-	      
-      	$scope.getClientesPorId = function(id){
-        	return _.find($scope.clientes, function(item){
-            	return item.id === id;
-        	})
-        };
+               $scope.consultarClientes = function() {
+                    var token = sessionStorage.getItem("user_session") || localStorage.getItem("user_session");
+                    if (token) {
+                         $http({
+                              url: $rootScope.api + '/cliente',
+                              method: 'GET',
+                              headers: {
+                                   'Authorization': token
+                              },
+                         }).success(function(response) {
+                              $scope.clientes = response;
+                              console.log(response);
+                         }).error(function(response) {
+                              $mdToast.show($mdToast.simple()
+                                   .content(response.message)
+                                   .hideDelay(3000));
+                              console.log(response);
+                              if (response.status == 2) window.location = "/#/login";
+                         });
+                    } else {
+                         console.log("sem token");
+                         window.location = "/#/login";
+                    }
+               };
 
-	}]
-);
+               $scope.getClientesPorId = function(id) {
+                    return _.find($scope.clientes, function(item) {
+                         return item.id === id;
+                    })
+               };
+
+          }
+     ]);
