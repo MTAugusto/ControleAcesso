36feca3249476110dda86e380c73c893ac22fea2
Lucas Montanheiro <lucasmontanheiro10@gmail.com>
2017-05-23 22:39:47 -0300

    Caixa Diario API ok

--- BackEnd/api/persistencia/CaixaDiarioDAO.php
+++ BackEnd/api/persistencia/CaixaDiarioDAO.php
@@ -50,55 +50,66 @@
 		header('Content-Type: application/json');
 		echo json_encode($response);
 	}
-	function delete($id)
-	{
-		global $connection;
-		$query="DELETE FROM caixadiarios WHERE id=".$id;
-		if(mysqli_query($connection, $query))
-		{
-			$response=array(
-				'status' => 1,
-				'message' =>'Deletado com sucesso.'
-			);
-		}
-		else
-		{
-			$response=array(
-				'status' => 0,
-				'message' =>'Houve um erro ao deletar.'
-			);
-			header("HTTP/2.0 400 Bad Request");
-		}
-		header('Content-Type: application/json');
-		echo json_encode($response);
-	}
 	function update()
 	{
 		global $connection;
 		parse_str(file_get_contents("php://input"),$post_vars);
 		$id=$post_vars['id'];
-		$valortotal=$post_vars["valortotal"];
 		$fechamento = date("Y-m-d H:i:s");
 		$usuario = getIdUser();
-		$query="UPDATE caixadiarios SET fechamento='{$fechamento}', valortotal='{$valortotal}', isfechado=1 WHERE id=$id AND usuario=$usuario";
-		if(mysqli_query($connection, $query))
+
+		$query1="select sum(valor) AS valortotal from movimentacao_caixadiario mc join saidas_veiculos sv on mc.saida_veiculo = sv.id  where caixadiario = $id";
+		$caixa=array();
+		$result=mysqli_query($connection, $query1);
+		while($row=mysqli_fetch_object($result))
+		{
+			$caixa[]=$row;
+		}
+
+		$valorFechamento = $caixa[0]->valortotal;
+
+		$query2="UPDATE caixadiarios SET fechamento='{$fechamento}', valortotal='{$valorFechamento}', isfechado=1 WHERE id=$id AND usuario=$usuario";
+		if(mysqli_query($connection, $query2))
 		{
 			$response=array(
 				'status' => 1,
-				'message' =>'Atualizado com sucesso.'
+				'message' =>'Caixa fechado com sucesso.'
 			);
 		}
 		else
 		{
 			$response=array(
 				'status' => 0,
-				'message' =>'Houve um erro ao atualizar.'
+				'message' =>'Houve um erro ao fechar o caixa.'
 			);
 			header("HTTP/2.0 400 Bad Request");
 		}
+
 		header('Content-Type: application/json');
 		echo json_encode($response);
 	}
+	// 	function delete($id)
+	// {
+	// 	global $connection;
+	// 	$query="DELETE FROM caixadiarios WHERE id=".$id;
+	// 	if(mysqli_query($connection, $query))
+	// 	{
+	// 		$response=array(
+	// 			'status' => 1,
+	// 			'message' =>'Deletado com sucesso.'
+	// 		);
+	// 	}
+	// 	else
+	// 	{
+	// 		$response=array(
+	// 			'status' => 0,
+	// 			'message' =>'Houve um erro ao deletar.'
+	// 		);
+	// 		header("HTTP/2.0 400 Bad Request");
+	// 	}
+	// 	header('Content-Type: application/json');
+	// 	echo json_encode($response);
+	// }
 
 	// Close database connection
 	mysqli_close($connection);
