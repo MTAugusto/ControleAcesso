8414b9fefe8c29d9d77fa8fa1c3d869eb498ccdc
Mateus Augusto <mateus_2d@hotmail.com>
2017-06-07 10:50:03 -0300

    Remoção de Console.log

--- web_estacionamento/painel/js/acessibilidade.js
+++ web_estacionamento/painel/js/acessibilidade.js
@@ -1,16 +1,12 @@
 function aplicaAutoContraste() {
 
      let contraste = document.getElementById("autocontraste");
-     console.log(contraste);
      let splitUrl = contraste.href.split("/");
-     console.log(splitUrl[5]);
 
      if (splitUrl[5] == "styles.css") {
           contraste.href = 'css/autocontraste.css'
-          console.log(contraste);
      } else {
           contraste.href = 'css/styles.css'
-          console.log(contraste);
      }
 
 }
