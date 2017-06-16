b00c834fb4026cf4ec800aeb6205de6a552225f2
Mateus Augusto <mateus_2d@hotmail.com>
2017-06-16 14:26:43 -0300

    Alterações de acessibilidade.
    Foi inserido novo icone de acessibilidade para alteração de fonte, inserção de tooltip para listar atalhos do sistema e correção de icone em tela cheia.

--- painel/js/menuItemsCtrl.js
+++ painel/js/menuItemsCtrl.js
@@ -4,71 +4,83 @@
            var token = sessionStorage.getItem("user_session") || localStorage.getItem("user_session");
            var isAdmin = JSON.parse(atob((token.split("."))[1])).admin;
 
-          if (isAdmin == 1) {
-            $scope.MenuItems = [{
-                     name: 'Início',
-                     link: '#/',
-                     icon: './img/ic_home_black_24px.svg',
-                     accesskey: 'I'
-                },
-                {
-                     name: 'Caixa Diário',
-                     link: '#/caixa-diario',
-                     icon: './img/ic_attach_money_black_24px.svg',
-                     accesskey: 'A'
-                },
-                //{name:'Administração', link:'#/administracao', icon:'vendas'},
-                {
-                     name: 'Usuários',
-                     link: '#/usuarios',
-                     icon: './img/ic_person_add_black_24px.svg',
-                     accesskey: 'U'
+           if (isAdmin == 1) {
+                $scope.MenuItems = [{
+                          name: 'Início',
+                          link: '#/',
+                          icon: './img/ic_home_black_24px.svg',
+                          accesskey: 'I',
+                          tooltip: 'Atalho para tela Inicial Alt + I'
+                     },
+                     {
+                          name: 'Caixa Diário',
+                          link: '#/caixa-diario',
+                          icon: './img/ic_attach_money_black_24px.svg',
+                          accesskey: 'A',
+                          tooltip: 'Atalho para tela Caixa Diário Alt + A'
+                     },
+                     //{name:'Administração', link:'#/administracao', icon:'vendas'},
+                     {
+                          name: 'Usuários',
+                          link: '#/usuarios',
+                          icon: './img/ic_person_add_black_24px.svg',
+                          accesskey: 'U',
+                          tooltip: 'Atalho para tela Usuários Alt + U'
 
-                },
-                {
-                     name: 'Clientes',
-                     link: '#/clientes',
-                     icon: './img/ic_group_add_black_24px.svg',
-                     accesskey: 'C'
-                },
-                {
-                     name: 'Tipos',
-                     link: '#/tipos',
-                     icon: './img/ic_playlist_add_black_24px.svg',
-                     accesskey: 'T'
-                },
-                {
-                     name: 'Veículos',
-                     link: '#/veiculos',
-                     icon: './img/ic_directions_car_black_24px.svg',
-                     accesskey: 'V'
-                }];
-          } else {
-            $scope.MenuItems = [{
-                     name: 'Início',
-                     link: '#/',
-                     icon: './img/ic_home_black_24px.svg',
-                     accesskey: 'I'
-                },
-                {
-                     name: 'Caixa Diário',
-                     link: '#/caixa-diario',
-                     icon: './img/ic_attach_money_black_24px.svg',
-                     accesskey: 'A'
-                },
-                {
-                     name: 'Clientes',
-                     link: '#/clientes',
-                     icon: './img/ic_group_add_black_24px.svg',
-                     accesskey: 'C'
-                },
-                {
-                     name: 'Veículos',
-                     link: '#/veiculos',
-                     icon: './img/ic_directions_car_black_24px.svg',
-                     accesskey: 'V'
-                }];
-          }
+                     },
+                     {
+                          name: 'Clientes',
+                          link: '#/clientes',
+                          icon: './img/ic_group_add_black_24px.svg',
+                          accesskey: 'C',
+                          tooltip: 'Atalho para tela Clientes Alt + C'
+                     },
+                     {
+                          name: 'Tipos',
+                          link: '#/tipos',
+                          icon: './img/ic_playlist_add_black_24px.svg',
+                          accesskey: 'T',
+                          tooltip: 'Atalho para tela Tipos Alt + T'
+                     },
+                     {
+                          name: 'Veículos',
+                          link: '#/veiculos',
+                          icon: './img/ic_directions_car_black_24px.svg',
+                          accesskey: 'V',
+                          tooltip: 'Atalho para tela Veículos Alt + V'
+                     }
+                ];
+           } else {
+                $scope.MenuItems = [{
+                          name: 'Início',
+                          link: '#/',
+                          icon: './img/ic_home_black_24px.svg',
+                          accesskey: 'I',
+                          tooltip: 'Atalho para tela Inicial Alt + I'
+                     },
+                     {
+                          name: 'Caixa Diário',
+                          link: '#/caixa-diario',
+                          icon: './img/ic_attach_money_black_24px.svg',
+                          accesskey: 'A',
+                          tooltip: 'Atalho para tela Caixa Diário Alt + A'
+                     },
+                     {
+                          name: 'Clientes',
+                          link: '#/clientes',
+                          icon: './img/ic_group_add_black_24px.svg',
+                          accesskey: 'C',
+                          tooltip: 'Atalho para tela Clientes Alt + C'
+                     },
+                     {
+                          name: 'Veículos',
+                          link: '#/veiculos',
+                          icon: './img/ic_directions_car_black_24px.svg',
+                          accesskey: 'V',
+                          tooltip: 'Atalho para tela Veículos Alt + V'
+                     }
+                ];
+           }
 
 
            $scope.sair = function() {
